30d4fb1dc05dd406df6ddc8ecbc04eb4da7ba407
MasoomRaj <raazsri07@gmail.com>
2024-04-15 13:00:56 +0530

    my first project

---
+++ project4.js
@@ -0,0 +1,13 @@
+document.querySelector('button').addEventListener('click',function(){
+    document.querySelector('#ut').style.position='absolute'
+    document.querySelector('#ut').style.left='5px'
+
+
+
+})
+document.querySelector('#ul').addEventListener('click',function(){
+
+    document.querySelector('#ut').style.position='absolute';
+    document.querySelector('#ut').style.left='-450px';
+
+})
